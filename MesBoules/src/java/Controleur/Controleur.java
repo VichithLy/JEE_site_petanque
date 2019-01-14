d7cf4aa1983edf8842b3b7c14da747ca162290ad
NARAYANIN-RICHENAPIN ALEC p1619278 <alec.narayanin-richenapin@etu.univ-lyon1.fr>
2019-01-14 11:54:43 +0100

    Ajout jsp header header_co et footer

--- MesBoules/src/java/Controleur/Controleur.java
+++ MesBoules/src/java/Controleur/Controleur.java
@@ -36,7 +36,7 @@ public class Controleur extends HttpServlet {
         String todo = request.getParameter("todo");
         RequestDispatcher rd =null;
         Action ac = null;
-        String vue="Accueil.jsp";
+        String vue="Accueil.jsp";git pull
         switch(todo){
             
             case "connexion" :
