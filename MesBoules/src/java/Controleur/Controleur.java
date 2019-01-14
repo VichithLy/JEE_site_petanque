96c7bedb98c6b075d35b85f9676830c28d10ea01
NARAYANIN-RICHENAPIN ALEC p1619278 <alec.narayanin-richenapin@etu.univ-lyon1.fr>
2019-01-14 11:27:35 +0100

    Structuration du controleur

--- MesBoules/src/java/Controleur/Controleur.java
+++ MesBoules/src/java/Controleur/Controleur.java
@@ -5,10 +5,96 @@
  */
 package Controleur;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  *
- * @author p1700906
+ * @author p1619278
  */
-public class Controleur {
-    
+@WebServlet(name = "Controleur", urlPatterns = {"/Controleur"})
+public class Controleur extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+   protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       
+        String todo = request.getParameter("todo");
+        RequestDispatcher rd =null;
+        Action ac = null;
+        String vue="index.html";
+        switch(todo){
+            
+            case "" :
+                
+                rd = request.getRequestDispatcher(vue);
+                break;
+                
+            case "" :
+                
+                rd = request.getRequestDispatcher(vue);
+                break;
+                
+            default :
+                rd = request.getRequestDispatcher(vue);
+                
+                
+        }
+        rd.forward(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
 }
